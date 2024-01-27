cdcf1a283dbc52cb8350eed6e88f021ea55ee97d
Jacob Rogers <rogersair2@aol.com>
2024-01-27 13:00:26 -0500

    movie cards

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import Navbar from "@/components/Navbar";
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hooks/useMovieList";
 
 export default function Home() {
 
@@ -14,10 +16,15 @@ export default function Home() {
     },
   });
 
+  const {data: movies = [] } = useMovieList();
+
   return (
     <>
       <Navbar />
-      <Billboard />      
+      <Billboard />
+      <div className="pb-40">
+         <MovieList title="Trending Now" data={movies}/> 
+      </div>      
     </>
   );
 }
