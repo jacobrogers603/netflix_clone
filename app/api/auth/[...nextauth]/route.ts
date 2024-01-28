6d943d32806cd0ba13846d8387bbaaea26a6b44a
Jacob Rogers <rogersair2@aol.com>
2024-01-27 22:35:07 -0500

    cant build app/api/auth/[...nextauth]/route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,79 +1,80 @@
-import NextAuth from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-// Don't forget this import!
+import NextAuth from 'next-auth/next';
 import prismadb from '@/lib/prismadb';
-import {compare} from 'bcrypt';
+
+import Credentials from 'next-auth/providers/credentials';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import { compare } from 'bcrypt';
 import { AuthOptions } from 'next-auth';
 
 export const authOptions: AuthOptions = {
-    providers: [
-        GithubProvider({
-            clientId: process.env.GITHUB_ID || '',
-            clientSecret: process.env.GITHUB_SECRET || ''
-        }),
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID || '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
-        }),
-        Credentials({
-            id: 'credentials',
-            name: 'Credentials',
-            credentials: {
-                email: {
-                    label: 'Email',
-                    type: 'text'
-                },
-                password: {
-                    label: 'Password',
-                    type: 'password'
-                }
-            },
-            async authorize(credentials) {
-                // check if we have credentials or not.
-                if(!credentials?.email || !credentials?.password){
-                    throw new Error('Email and password required.');
-                }
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    Credentials({
+      id: 'credentials',
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error('Email and password are required');
+        }
 
-                // Find the user using email from in our data base.
-                const user = await prismadb.user.findUnique({
-                    where: {
-                        email: credentials.email
-                    }
-                });
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
 
-                // Check if the user exists.
-                if(!user || !user.hashedPassword){
-                    throw new Error('Email does not exist.');
-                }
+        if (!user || !user.hashedPassword) {
+          throw new Error('Email does not exist');
+        }
 
-                // Check if the password the user entered is correct, if so return user.
-                const isCorrectPassword = await compare(credentials.password, user.hashedPassword);   
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword as string
+        );
+        if (!isCorrectPassword) {
+          throw new Error('Invalid password');
+        }
 
-                if(!isCorrectPassword){
-                    throw new Error('Incorrect password.');
-                }
+        return user;
+      },
+    }),
+  ],
 
-                return user;
-            }
-        })
-    ],
-    // Our auth page.
-    pages: {
-        signIn: '/auth'
-    },
-    // Turns on needed logs and errors in terminal when developing.
-    debug: process.env.NODE_ENV === 'development',
-    adapter: PrismaAdapter(prismadb),
-    session: {
-        strategy: 'jwt'
-    },
-    jwt: {
-        secret: process.env.NEXTAUTH_JWT_SECRET
-    },
-    secret: process.env.NEXTAUTH_SECRET
+  pages: {
+    signIn: '/auth'
+  },
+  debug: process.env.NODE_ENV === 'development',
+  adapter: PrismaAdapter(prismadb),
+  session: {
+    strategy: 'jwt',
+  },
+  jwt: {
+    secret: process.env.NEXTAUTH_JWT_SECRET,
+  },
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 const handler = NextAuth(authOptions);
